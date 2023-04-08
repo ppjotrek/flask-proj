92383183dec1bafb06f17e8bce0b4e2a2940d404
Piotrek <99807592+ppjotrek@users.noreply.github.com>
2023-04-08 12:21:29 +0200

    first html template

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+items = ["Pierwszy element", "Drugi element", "Trzeci element"]
+
+@app.route('/items')
+def show_items():
+    return render_template("items.html", items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
